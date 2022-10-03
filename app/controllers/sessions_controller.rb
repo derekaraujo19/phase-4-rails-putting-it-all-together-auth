3c954f40188ce343f4a899e4d798459ced33b8bc
Derek Araujo <derekaraujo19@gmail.com>
2022-10-02 22:50:19 -0400

    lab complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  # Log In
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: {errors: ["Invalid username or password"]}, status: :unauthorized
+    end
+  end
+
+  # Log Out
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+
+end
+
+
+
