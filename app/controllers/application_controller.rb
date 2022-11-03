164044ddfd5d1087292714db570a126a2487b54a
Derek Araujo <derekaraujo19@gmail.com>
2022-11-03 19:01:19 -0400

    commit tree 7b55bbe0846c11e65405dbf7d30d01494f47ca40

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,64 @@ class ApplicationController < ActionController::API
   before_action :authorize
 
 
+
   private
+
 
   def render_unprocessable_entity_response(exception)
     render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
   end
 
   def authorize
-    render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+    render json: {errors: ["Not Authorized"]}, status: :unauthorized unless session.include? :user_id
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  # before_action :authorize
+
+
+  # private
+
+  # def render_unprocessable_entity_response(exception)
+  #   render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  # end
+
+  # def authorize
+  #   render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+  # end
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,74 @@
 class UsersController < ApplicationController
+
   skip_before_action :authorize, only: :create
 
-  # Sign Up
+  # create an account
   def create
     user = User.create!(user_params)
     session[:user_id] = user.id
     render json: user, status: :created
   end
 
-  # Stay Logged In
+  # Staying logged in
   def show
     user = User.find_by(id: session[:user_id])
     render json: user, status: :created
   end
 
+
+
+
+
+
+
+
+
+
+
+
+
   private
 
   def user_params
-    params.permit(:username, :password, :image_url, :bio, :password_confirmation)
+    params.permit(:username, :password, :password_confirmation, :image_url, :bio)
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # skip_before_action :authorize, only: :create
+
+  # # Sign Up
+  # def create
+  #   user = User.create!(user_params)
+  #   session[:user_id] = user.id
+  #   render json: user, status: :created
+  # end
+
+  # # Stay Logged In
+  # def show
+  #   user = User.find_by(id: session[:user_id])
+  #   render json: user, status: :created
+  # end
+
+  # private
+
+  # def user_params
+  #   params.permit(:username, :password, :image_url, :bio, :password_confirmation)
+  # end
 
 
 
